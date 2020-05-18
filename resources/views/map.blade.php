05cc9ed2fba408e0f52b41706752d5f1c1b20cbd
Gabriel Poma <gpoma@24eme.fr>
2020-05-18 16:47:40 +0200

    map.css et panel d'info

--- resources/views/map.blade.php
+++ resources/views/map.blade.php
@@ -4,11 +4,12 @@
     @parent
 
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css" integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ==" crossorigin=""/>
-    <style>#mapid {height: 100%; width: 100%;}</style>
+    <link rel="stylesheet" href="/css/map.css">
 @endsection
 
 @section('content')
     <div id="mapid"></div>
+    <div id="info-panel"></div>
     <script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js" integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew==" crossorigin=""></script>
 
     <script src="/js/map.js"></script>
