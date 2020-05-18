ab6a7e6bd6c9f33588b287ec0f778600528e1f97
Gabriel Poma <gpoma@24eme.fr>
2020-05-18 18:33:37 +0200

    changement fond de carte

--- public/js/map.js
+++ public/js/map.js
@@ -1,8 +1,7 @@
 var map = L.map('mapid').setView([48.8534100, 2.3488000], 13)
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
-    maxZoom: 18,
-    tileSize: 512,
-    zoomOffset: -1,
+L.tileLayer('https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}{r}.png', {
+    attribution: '<a href="https://wikimediafoundation.org/wiki/Maps_Terms_of_Use">Wikimedia</a>',
+    maxZoom: 19,
+    minZoom: 1
 }).addTo(map)
