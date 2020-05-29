495f051c4407ba57ebd80a3e08ed2b2af852f1ce
Gabriel Poma <gpoma@24eme.fr>
2020-05-29 12:58:49 +0200

    menu page des lieux

---
+++ resources/views/components/place-menu.blade.php
@@ -0,0 +1,11 @@
+<div class="section" id="scrolling-menu">
+    <aside class="menu">
+        <p class="menu-label">Menu</p>
+        <ul class="menu-list">
+            <li><a href="#presentation">Présentation du lieu</a></li>
+            <li><a href="#federation">Fédération</a></li>
+            <li><a href="#analyse">Collecte et analyse</a></li>
+            <li><a href="#bilan">Bilan</a></li>
+        </ul>
+    </aside>
+</div>
