2f0a07236477e776fbb7c5cae7a1d48fcb2faaf5
Gabriel Poma <gpoma@24eme.fr>
2020-06-18 15:37:11 +0200

    Nom dans infobox + map sous info

--- resources/views/components/place/info-box.blade.php
+++ resources/views/components/place/info-box.blade.php
@@ -1,15 +1,14 @@
 <aside id="info-box" class="scrolling-menu">
-    <h3 class="info-box-header">Localisation</h3>
-    <div class="info-box-content">
-        <div id="info-box-map" class="info-box-map"></div>
-        <dl>
-            <dt><a href="geo:{{ $place->geo->lat }},{{ $place->geo->lon }}">Adresse</a></dt>
-            <dd>{{ $place->address->address }}, {{ $place->address->postalcode }} {{ $place->address->city }}</dd>
-        </dl>
-    </div>
     <h3 class="info-box-header">Informations</h3>
     <div class="info-box-content">
         <div class="columns is-multiline is-gapless">
+            <div class="column is-half">
+                <span class="info-box-entry">Nom</span>
+            </div>
+            <div class="column is-half">
+                {{ $place->name }}
+            </div>
+
             <div class="column is-half">
                 <span class="info-box-entry">Création</span>
             </div>
@@ -57,6 +56,14 @@
                 {{ $place->status }}
             </div>
         </div>
+    </div>
+    <h3 class="info-box-header">Localisation</h3>
+    <div class="info-box-content">
+        <div id="info-box-map" class="info-box-map"></div>
+        <dl>
+            <dt><a href="geo:{{ $place->geo->lat }},{{ $place->geo->lon }}">Adresse</a></dt>
+            <dd>{{ $place->address->address }}, {{ $place->address->postalcode }} {{ $place->address->city }}</dd>
+        </dl>
     </div>
     <h3 class="info-box-header">Réseaux sociaux</h3>
     <div class="info-box-content columns is-multiline has-text-centered is-gapless">
