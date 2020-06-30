8654e90c8f9c38b4a8e3c8d27ca4b19a6e2d2569
Gabriel Poma <gpoma@24eme.fr>
2020-06-30 18:04:28 +0200

    tabs pour graph 'les données'

--- resources/views/impacts/datas.blade.php
+++ resources/views/impacts/datas.blade.php
@@ -9,31 +9,33 @@
 @endsection
 @section('content')
   <div class="container">
-      <div class="hero is-large is-light">
-          <div class="section">
-            <div class="columns">
-                <div class="column">
-                  <div class="columns">
-                      <figure class="image is-128x128">
-                        <img src="{{ url('/images/datas.svg') }}" >
-                      </figure>
-
-                      <h1 class="title is-1 has-text-centered">Les données</h1>
-                  </div>
-
-                </div>
-                <div class="column  is-three-fifths ">
-                  <p>Hopla vous savez que la mamsell Huguette, la miss Miss Dahlias du messti de Bischheim était au <a href="#">Christkindelsmärik</a> en compagnie de Richard Schirmeck (celui qui a un blottkopf), le mari de Chulia Roberstau, qui lui trempait sa Nüdle dans sa Schneck ! Yo dû, Pfourtz ! Ch'espère qu'ils avaient du Kabinetpapier, Gal !</p>
-                  <p>Tu restes pour le lotto-owe ce soir, y'a baeckeoffe ? Yeuh non, merci vielmols mais che dois partir à la Coopé de Truchtersheim acheter des mänele et des rossbolla pour les gamins. Hopla tchao bissame ! Consectetur adipiscing elit</p>
-                  <p>Hopla vous savez que la mamsell Huguette, la miss Miss Dahlias</p>
-                  <p>Tu restes pour le lotto-owe ce soir, y'a baeckeoffe ? Yeuh non, merci</p>
-                </div>
+      <div class="section">
+        <div class="columns">
+            <div class="column">
+                <h1 class="title is-1 has-text-centered">Les données</h1>
+                <figure class="image is-128x128">
+                    <img src="{{ url('/images/datas.svg') }}" >
+                </figure>
             </div>
 
+            <div class="content column is-three-fifths">
+                <p>Hopla vous savez que la mamsell Huguette, la miss Miss Dahlias du messti de Bischheim était au <a href="#">Christkindelsmärik</a> en compagnie de Richard Schirmeck (celui qui a un blottkopf), le mari de Chulia Roberstau, qui lui trempait sa Nüdle dans sa Schneck ! Yo dû, Pfourtz ! Ch'espère qu'ils avaient du Kabinetpapier, Gal !</p>
+                <p>Tu restes pour le lotto-owe ce soir, y'a baeckeoffe ? Yeuh non, merci vielmols mais che dois partir à la Coopé de Truchtersheim acheter des mänele et des rossbolla pour les gamins. Hopla tchao bissame ! Consectetur adipiscing elit</p>
+                <p>Hopla vous savez que la mamsell Huguette, la miss Miss Dahlias</p>
+                <p>Tu restes pour le lotto-owe ce soir, y'a baeckeoffe ? Yeuh non, merci</p>
+            </div>
+        </div>
+      </div>
+
+      <div class="section">
+        <div class="field is-horizontal">
+          <div class="field-label is-normal">
+            <label for="resilience-select" class="label">Choisissez un indicateur:</label>
+          </div>
+          <div class="field-body">
             <div class="field">
               <div class="control">
-                <label for="resilience-select" class="title">Choisissez un indicateur:</label>
-                <div class="select is-small is-success" style="margin-top:1em;">
+                <div class="select is-small is-success">
                   <select name="resilience" id="resilience-select" class="is-focused" onchange="createResilienceBar(this)">
                       @foreach($places[0]->data->resilience as $type_resilience => $resilience)
                         <option value="{{ $type_resilience }}">{{ $resilience->title }}</option>
@@ -43,8 +45,30 @@
               </div>
             </div>
           </div>
-          <div class="section" id="sectionResilienceBar">
-          </div>
+        </div>
+
+        <div class="tabs is-right is-small" data-tab-group="indicateurs">
+          <ul>
+            <li class="is-active">
+              <a href="#charts">
+                <span class="icon is-small"><i class="fas fa-chart-line" aria-hidden="true"></i></span>
+                <span>Graphiques</span>
+              </a>
+            </li>
+            <li>
+              <a href="#raw">
+                <span class="icon is-small"><i class="fas fa-table" aria-hidden="true"></i></span>
+                <span>Données</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div class="tabs-content" data-tab-group="indicateurs">
+            <div class="tab is-active" data-tab="charts">
+                <div class="section" id="sectionResilienceBar">
+                </div>
+            </div>
+        </div>
       </div>
   </div>
 @endsection
