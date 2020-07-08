62ea02a980d598e3bdbebd48b1c4632ef2262c6a
Gabriel Poma <gpoma@24eme.fr>
2020-07-08 17:11:43 +0200

    Indicateurs d'approche dans la partie approche

--- resources/views/components/place-menu.blade.php
+++ resources/views/components/place-menu.blade.php
@@ -3,7 +3,8 @@
         <p class="menu-label">Menu</p>
         <ul class="menu-list">
             <li><a href="#presentation">Présentation du lieu</a></li>
-            <li><a href="#indicateurs">Indicateurs</a></li>
+            <li><a href="#indicateurs-martin">Indicateurs de Martin</a></li>
+            <li><a href="#indicateurs-approche">Indicateurs d'Approche</a></li>
             <li><a href="#donnees-insee">Données INSEE</a></li>
         </ul>
     </aside>
