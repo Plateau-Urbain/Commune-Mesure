a15848ba0a82c7857a2fc727fe9a8f1fae348826
Gabriel Poma <gpoma@24eme.fr>
2020-10-13 18:58:25 +0200

    clean js inutile

--- resources/views/components/place/insee-chart-js.blade.php
+++ resources/views/components/place/insee-chart-js.blade.php
@@ -1,6 +1,3 @@
-<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
-
 <script>
 
 var options = {
