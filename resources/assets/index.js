8e80b2e6414ed3979a23a8287a1ee59827c74a42
Vincent LAURENT <vince.laurent@gmail.com>
2020-10-14 18:25:06 +0200

    webpack icon leaflet

--- resources/assets/index.js
+++ resources/assets/index.js
@@ -4,6 +4,13 @@ import * as L from 'leaflet';
 import 'leaflet.markercluster';
 import { GestureHandling } from 'leaflet-gesture-handling';
 
+L.Icon.Default.imagePath = '/';
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+    iconUrl: require('leaflet/dist/images/marker-icon.png'),
+    shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+});
+
 import Chart from 'chart.js';
 import './js/tabs.js';
 
