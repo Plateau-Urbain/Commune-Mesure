769ecd0031ae42c9978a0e022fe469d60629c59b
Vincent LAURENT <vince.laurent@gmail.com>
2020-10-14 18:20:59 +0200

    Pas d'icon custom

--- resources/assets/index.js
+++ resources/assets/index.js
@@ -4,16 +4,6 @@ import * as L from 'leaflet';
 import 'leaflet.markercluster';
 import { GestureHandling } from 'leaflet-gesture-handling';
 
-import icon from 'leaflet/dist/images/marker-icon.png';
-import iconShadow from 'leaflet/dist/images/marker-shadow.png';
-
-let DefaultIcon = L.icon({
-    iconUrl: icon,
-    shadowUrl: iconShadow
-});
-
-L.Marker.prototype.options.icon = DefaultIcon;
-
 import Chart from 'chart.js';
 import './js/tabs.js';
 
