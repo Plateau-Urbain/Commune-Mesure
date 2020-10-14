6ac57e0d590167ecddf9dde1ed6514e09da6484e
Gabriel Poma <gpoma@24eme.fr>
2020-10-14 10:34:43 +0200

    on enleve lorem ipsum page stats

--- resources/views/impacts/statistics.blade.php
+++ resources/views/impacts/statistics.blade.php
@@ -1,18 +1,7 @@
 <div class="content">
 <div class="hero is-large is-light">
   <div class="section container is-fullhd">
-    <div class="columns">
-    <div class="column" id="descriptionStatistic">
-              <div class="columns">
-                <figure class="image is-128x128"><img src="{{ url('/images/statistics.svg') }}" ></figure>
-                <h1 class="title is-1 has-text-centered">Les statistiques</h1>
-              </div>
-    </div>
-    <div class="column  is-three-fifths ">
-              <p>Hopla vous savez que la mamsell Huguette, la miss Miss Dahlias du messti de Bischheim était au <a href="#">Christkindelsmärik</a> en compagnie de Richard Schirmeck (celui qui a un blottkopf), le mari de Chulia Roberstau, qui lui trempait sa Nüdle dans sa Schneck ! Yo dû, Pfourtz ! Ch'espère qu'ils avaient du Kabinetpapier, Gal !</p>
-              <p>Tu restes pour le lotto-owe ce soir, y'a baeckeoffe ? Yeuh non, merci vielmols mais che dois partir à la Coopé de Truchtersheim acheter des mänele et des rossbolla pour les gamins. Hopla tchao bissame ! Consectetur adipiscing elit</p>
-    </div>
-    </div>
+      <h1 class="title is-1 has-text-centered">Les statistiques</h1>
   </div>
   <div class="section has-text-centered font-color-theme pt-0">
     <div class="columns is-mobile">
