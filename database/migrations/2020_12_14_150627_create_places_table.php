cbd089a08e5d32345522be2bdc1ad04cd55b066f
Gabriel Poma <gpoma@24eme.fr>
2020-12-14 16:25:58 +0100

    migration sqlite

---
+++ database/migrations/2020_12_14_150627_create_places_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Query\Expression;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePlacesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('places', function (Blueprint $table) {
+            $table->uuid('id');
+            $table->string('place')->unique();
+            $table->json('data')->default(new Expression('(JSON_ARRAY())'));
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->index('place');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('places');
+    }
+}
