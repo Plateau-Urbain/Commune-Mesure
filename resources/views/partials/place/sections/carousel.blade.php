5ab290e2c9a5411844a84d982ca62efe6889e755
Gabriel Poma <gpoma@24eme.fr>
2021-02-18 18:57:15 +0100

    fix container liste lieu

--- resources/views/partials/place/sections/carousel.blade.php
+++ resources/views/partials/place/sections/carousel.blade.php
@@ -1,4 +1,4 @@
-<div class="container carousel-container column is-6">
+<div class="container carousel-container with-padding column is-6">
   <h2 class="ribbon-banner is-5 has-text-centered">Gallerie</h2>
   <div id="place-carousel" class="carousel" data-navigation=1 data-infinite=true>
     <div class="card item-1">
