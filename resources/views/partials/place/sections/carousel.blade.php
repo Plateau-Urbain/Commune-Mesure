474d2e5fc34e5b79b9c6c611283bbd0be2decbb2
Gabriel Poma <gpoma@24eme.fr>
2021-02-18 18:04:23 +0100

    un bout de bill murray a gauche et droite

--- resources/views/partials/place/sections/carousel.blade.php
+++ resources/views/partials/place/sections/carousel.blade.php
@@ -1,6 +1,6 @@
-<div class="container column is-6">
+<div class="container carousel-container column is-6">
   <h2 class="ribbon-banner is-5 has-text-centered">Gallerie</h2>
-  <div id="place-carousel" class="carousel" data-navigation=1>
+  <div id="place-carousel" class="carousel" data-navigation=1 data-infinite=true>
     <div class="card item-1">
       <figure class="image is-covered">
         <img src="https://www.fillmurray.com/500/250">
