65bcb1bf9c851eb1bdaa91c514dadf2334ad8a80
Honorine-MC <honorine.mariechristy@gmail.com>
2021-02-24 12:20:32 +0100

    vue pour lieu non publié

---
+++ resources/views/partials/place/not-published.blade.php
@@ -0,0 +1,9 @@
+@section('content')
+<div class="container">
+    <div class="hero is-large is-light">
+        <section class="section">
+                <h1 class="title is-1 has-text-centered">Le lieu n'est pas encore publié</h1>
+        </section>
+    </div>
+</div>
+@endsection
