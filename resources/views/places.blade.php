cdcaae23b87a9414757ab1e480e50ce4c25ff2cf
Honorine-MC <honorine.mariechristy@gmail.com>
2021-03-02 13:13:38 +0100

    listing des places completement avec le model

--- resources/views/places.blade.php
+++ resources/views/places.blade.php
@@ -12,7 +12,7 @@
                 <div class="box box-lieu content">
                     <div class="columns is-bordered places-block">
                         <div class="column is-clickable" style="position:relative; height:250px;">
-                            <p class="title mb-4"><a href="{{ route('place.show',['slug' => $place->url ]) }}">{{ $place->get('name') }}</a><br />
+                            <p class="title mb-4"><a href="{{ route('place.show',['slug' => $place->get('url') ]) }}">{{ $place->get('name') }}</a><br />
                             <span class="title_places-city is-size-4" style="font-weight: normal">{{ $place->get('city') }} ({{ substr($place->get('postalcode'), 0, 2) }})</span></p>
                             <p style="text-overflow: ellipsis; overflow: hidden; display: -webkit-box; -webkit-line-clamp: 3;-webkit-box-orient: vertical;">{{ (json_decode($place->get('description')))->value }}</p>
                             <ul class="tags_container">
@@ -20,14 +20,14 @@
                               <li class="tags">{{$tag}}</li>
                             @endforeach
                             </ul>
-                            <a href="{{ route('place.show',['slug' => $place->url ]) }}" class="btn-voir-lieu button is-default">Voir ce lieu</a>
+                            <a href="{{ route('place.show',['slug' => $place->get('url') ]) }}" class="btn-voir-lieu button is-default">Voir ce lieu</a>
                         </div>
                         <div class="column is-one-third has-text-centered">
-                            <div id="carousel-{{ $place->url }}" class="carousel carousel-container">
+                            <div id="carousel-{{ $place->get('url') }}" class="carousel carousel-container">
                             @if( count(json_decode($place->get('photos'))) > 0)
                               <img class="img-places" height="230px" src='{{ url("/") }}/images/lieux/{{ json_decode($place->get('photos'))[0] }}'>
                             @endif
-                              <div class="map-place" id="map_{{ $place->url }}"></div>
+                              <div class="map-place" id="map_{{ $place->get('url') }}"></div>
                             </div>
                         </div>
                     </div>
