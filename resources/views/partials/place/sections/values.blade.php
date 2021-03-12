f7d914bd808fd6594485696f69913f5d27feb580
Honorine-MC <honorine.mariechristy@gmail.com>
2021-03-12 18:46:19 +0100

    sections valeurs d'essai pour pouvoir faire l'édition avec les chemins

--- resources/views/partials/place/sections/values.blade.php
+++ resources/views/partials/place/sections/values.blade.php
@@ -1,7 +1,13 @@
 <h2 class="ribbon-banner title is-5 has-text-centered">Les valeurs</h2>
 <div class="columns">
   <div class="column has-text-centered">
-    <div id="value_container"></div>
+    @foreach($place->get('blocs->valeurs->donnees') as $valeur => $tabExemples)
+        <label style="color:red">{{$valeur}}</label>
+        @include('components.modals.modalEdition',['chemin'=>"blocs->valeurs->donnees->$valeur",'type'=>'text','titre'=>"Modifier Les valeurs",'description'=>"Donner des exemples en rapport avec le ".$valeur."?"])&nbsp; &nbsp;
+        @foreach($tabExemples as $exemple)
+          <p>{{$exemple}}</p>
+        @endforeach
+        <br>
+    @endforeach
   </div>
 </div>
-
