715fe04c0ea9860087d7c69b4f0f194ade090584
Honorine-MC <honorine.mariechristy@gmail.com>
2021-04-09 12:47:22 +0200

    centrer les exemples des valeurs en fonction du nombre de valeurs qui ont des exemples

--- resources/views/partials/place/sections/values.blade.php
+++ resources/views/partials/place/sections/values.blade.php
@@ -3,6 +3,18 @@
 </div>
 <br>
 <div class="columns">
+
+    @if(isset($edit))
+      @php $i= count((array)$place->get('blocs->valeurs->donnees')) @endphp
+    @else
+      @php $i=0; @endphp
+      @foreach($place->get('blocs->valeurs->donnees') as $valeur => $tabExemples)
+        @if(count($tabExemples) > 0)
+          @php $i++; @endphp
+        @endif
+      @endforeach
+    @endif
+
     @foreach($place->get('blocs->valeurs->donnees') as $valeur => $tabExemples)
     @if(count($tabExemples)>0 || isset($edit))
       <div class="column">
@@ -10,7 +22,7 @@
           <strong class="valeurs">{{$valeur}}</strong>
           @include('components.modals.modalEdition',['chemin'=>"blocs->valeurs->donnees->$valeur",'type'=>'text','id_section'=>'section03','titre'=>"Modifier Les valeurs",'description'=>"Donner des exemples en rapport avec la valeur : ".$valeur])&nbsp; &nbsp;
         <p>
-        <div class='valeurs-exemples'>
+        <div class='valeurs-exemples' style='width:{{$i}}0%;'>
           <ul>
           @foreach($tabExemples as $exemple)
             <li><strong>- {{$exemple}}</strong></li>
