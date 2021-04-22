a00d7955f61679affd350760bf782094dcab05ea
Honorine-MC <honorine.mariechristy@gmail.com>
2021-04-22 17:38:13 +0200

    ajout soutiens financiers

--- resources/views/partenaires.blade.php
+++ resources/views/partenaires.blade.php
@@ -26,25 +26,34 @@
                     <img class="logo_partenaires" src="{{ url('/images/partenaires/1200px-Ministe_re_de_la_transition_e_cologique_et_solidaire.svg.png') }}" alt="">
                 </div>
                 <div class="column partenaire_container">
+                  <p class="partenaires_text"><p>
+                  <img class="logo_partenaires" src="{{ url('/images/partenaires/Fondation_de_France.svg.png') }}" alt="">
+                </div>
+                <div class="column partenaire_container">
+                  <p class="partenaires_text"><p>
+                  <img class="logo_partenaires" src="{{ url('/images/partenaires/Copie de LogoPionnierLFI.png') }}" alt="">
+                </div>
+                <div class="column partenaire_container">
+                </div>
+              </div>
+            <h2 class="subtitle is-2 has-text-centered mt-6">Les soutiens financiers</h2>
+            <div class="columns mt-4 mb-6">
+                <div class="column partenaire_container">
+                </div>
+                <div class="column partenaire_container">
+                    <div>
+                      <p class="partenaires_text"><p>
+                      <img class="logo_partenaires" src="{{ url('/images/partenaires/Copie de LogoPionnierLFI.png') }}" alt="">
+                    </div>
+                </div>
+                <div class="column partenaire_container">
+                    <p class="partenaires_text"><p>
+                    <img class="logo_partenaires" src="{{ url('/images/partenaires/Banque_des_territoires.png') }}" alt="">
+                </div>
+                <div class="column partenaire_container">
                 </div>
                 </div>
-                <div class="columns">
-                    <div class="column partenaire_container">
-                    </div>
-                    <div class="column partenaire_container">
-                        <p class="partenaires_text"><p>
-                        <img class="logo_partenaires" src="{{ url('/images/partenaires/Fondation_de_France.svg.png') }}" alt="">
-                    </div>
-                    <div class="column partenaire_container">
-                        <p class="partenaires_text"><p>
-                        <img class="logo_partenaires" src="{{ url('/images/partenaires/Copie de LogoPionnierLFI.png') }}" alt="">
-                    </div>
-                    <div class="column partenaire_container">
-                        <p class="partenaires_text"><p>
-                    </div>
-                    <div class="column partenaire_container">
-                    </div>
-                </div>
+
             </div>
         </section>
     </div>
