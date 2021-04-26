b1308091642f3ef1773f08509692af400fdf04af
Honorine-MC <honorine.mariechristy@gmail.com>
2021-04-26 10:25:16 +0200

    nouveau texte pour la page non public

--- resources/views/place/unpublished.blade.php
+++ resources/views/place/unpublished.blade.php
@@ -4,8 +4,8 @@
 <div class="container">
     <div class="hero is-large is-light">
         <section class="section">
-                <h2 class="title is-1 has-text-centered">Ce lieu existe mais cette fiche lieu n'est pas encore publique</h2>
-                <p class='is-size-4'>Bientôt plus d'information sur ce site.</p>
+                <h2 class="title is-1 has-text-centered">Ce lieu existe mais cette fiche n'est pas encore publique</h2>
+                <p class='is-size-4'>Bientôt plus d'information.</p>
                 <p class='is-size-4'>Si vous participez à la vie de ce lieu, n'hésitez pas à prendre contact avec nous.</p>
         </section>
     </div>
