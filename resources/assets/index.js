2816059713443ea3d0913d143cb1c81655d13e8f
Gabriel Poma <gpoma@24eme.fr>
2021-07-20 19:04:04 +0200

    fix maps page lieux

--- resources/assets/index.js
+++ resources/assets/index.js
@@ -53,4 +53,25 @@ window.onload = (event) => {
     values.forEach(function (v) {
         animateValue(v)
     })
+
+    document.querySelectorAll("div.map-place").forEach(nodeMap => {
+        var id = nodeMap.getAttribute("id");
+        var mapnode = document.getElementById(id);
+        var namePlace = id.replace("map_", '');
+        if (!geoDataPlace[namePlace]) {
+            console.log(namePlace+" not found ("+id+")");
+            return;
+        }
+        var latLon = [geoDataPlace[namePlace].geo.lat, geoDataPlace[namePlace].geo.lon];
+        var mapPlace = mapjs.create(id, {
+            gestureHandling: false,
+            minZoom: 9,
+            maxZoom: 9,
+            zoomControl: false
+        })
+        L.marker(latLon).addTo(mapPlace)
+        mapPlace.setView(latLon, 9)
+        mapPlace.dragging.disable()
+    })
+
 }
--- resources/views/partials/places/map-js.blade.php
+++ resources/views/partials/places/map-js.blade.php
@@ -1,23 +1,3 @@
 <script>
   var geoDataPlace = @json($coordinates);
-  document.querySelectorAll("div.map-place").forEach(nodeMap => {
-    var id = nodeMap.getAttribute("id");
-    var mapnode = document.getElementById(id);
-    var namePlace = id.replace("map_", '');
-    if (!geoDataPlace[namePlace]) {
-        console.log(namePlace+" not found ("+id+")");
-        return;
-    }
-    var latLon = [geoDataPlace[namePlace].geo.lat, geoDataPlace[namePlace].geo.lon];
-    var mapPlace = mapjs.create(id, {
-        gestureHandling: false,
-        minZoom: 9,
-        maxZoom: 9,
-        zoomControl: false
-    })
-    L.marker(latLon).addTo(mapPlace)
-    mapPlace.setView(latLon, 9)
-    mapPlace.dragging.disable()
-  })
-
 </script>
