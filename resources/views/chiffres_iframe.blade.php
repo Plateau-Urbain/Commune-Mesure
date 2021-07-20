68312f8bcf8f210743ec69c9d232488840bc3d76
Gabriel Poma <gpoma@24eme.fr>
2021-07-20 15:22:03 +0200

    js useless dans iframe

--- resources/views/chiffres_iframe.blade.php
+++ resources/views/chiffres_iframe.blade.php
@@ -8,10 +8,7 @@
     <body>
       @include('partials.external.chiffres', ["stats" => $stats, "coordinates" => $coordinates])
       @section('script_js')
-         <script src="/js/global.js"></script>
-         <script src="/js/bundle.js"></script>
          <script src="/js/animate.js"></script>
-
       @show
     </body>
 </html>
