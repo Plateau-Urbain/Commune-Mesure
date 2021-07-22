3451aee8cea2b0bfa498b8a6b2728f738daae81a
Gabriel Poma <gpoma@24eme.fr>
2021-07-22 17:52:14 +0200

    commande pour scraper le blog cm

---
+++ app/Console/Commands/ScrapeCommuneMesure.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Support\Facades\Http;
+use Illuminate\Console\Command;
+use \DOMDocument;
+use \DOMXPath;
+
+class ScrapeCommuneMesure extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'scrape:footer';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = "Télécharge le footer distant et remplace le footer";
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $response = Http::get('https://communemesure.fr/blog/export');
+
+        $dom = new DOMDocument();
+        libxml_use_internal_errors(true);
+
+        $dom->loadHtml($response->body());
+        $xpath = new DOMXPath($dom);
+
+        $query = '//footer';
+
+        $footer = $xpath->query($query);
+
+
+    }
+}
