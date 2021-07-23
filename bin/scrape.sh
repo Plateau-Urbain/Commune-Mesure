de3e2c37143c9bb5c102f03f7591e545132cebaa
Gabriel Poma <gpoma@24eme.fr>
2021-07-23 10:54:37 +0200

    on récupère aussi le header

--- bin/scrape.sh
+++ bin/scrape.sh
@@ -2,9 +2,12 @@
 
 _HOST="https://communemesure.fr"
 _WORKINGDIR="$(dirname "$0")"
+_SECTIONS=("header" "footer")
 
-tempfile=`mktemp`
+for section in "${_SECTIONS[@]}"; do
+    tempfile=`mktemp`
 
-curl "${_HOST}/blog/export/" | tr -d '\n' | tr -d '\t' | sed 's#</footer>.*#</footer>#' | sed 's#.*<footer#<footer#' > "$tempfile"
+    curl "${_HOST}/blog/export/" | tr -d '\n' | tr -d '\t' | sed "s#</${section}>.*#</${section}>#" | sed "s#.*<${section}#<${section}#" > "$tempfile"
 
-mv "$tempfile" "$_WORKINGDIR/../resources/views/generate/footer.blade.php"
+    mv "$tempfile" "$_WORKINGDIR/../resources/views/generate/"${section}".blade.php"
+done
