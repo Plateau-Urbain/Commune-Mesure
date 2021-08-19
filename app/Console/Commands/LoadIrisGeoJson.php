9eaee4db6cff5b088f6f4107b02566a3a452e596
Tangui Morlier <tangui@tangui.eu.org>
2021-08-19 17:30:25 +0200

    On récupère les geojson des différentes strates géographiques

--- app/Console/Commands/LoadIrisGeoJson.php
+++ app/Console/Commands/LoadIrisGeoJson.php
@@ -6,7 +6,7 @@ use Illuminate\Console\Command;
 
 class LoadIrisGeoJson extends Command
 {
-  protected $signature = 'iris:load';
+  protected $signature = 'iris:load {adresse}';
   protected $description = "Load iris";
   public function __construct()
     {
@@ -15,6 +15,35 @@ class LoadIrisGeoJson extends Command
 
     public function handle()
     {
-        print_r("Hello");
+        $data = [];
+        $adresse = $this->argument('adresse');
+        $iris_json = json_decode(file_get_contents("https://pyris.datajazz.io/api/search/?geojson=true&q=".urlencode($adresse)));
+        $data['geo'] = [];
+        $data['geo']['lat'] = $iris_json->lat;
+        $data['geo']['lon'] = $iris_json->lon;
+        $data['geo']['geo_json'] = [];
+        $data['geo']['geo_json']['iris'] = ['type' => "Feature"];
+        $data['geo']['geo_json']['iris']['geometry'] = $iris_json->geometry;
+        $data['geo']['geo_json']['iris']['properties'] = $iris_json->properties;
+
+        $commune_json = json_decode(file_get_contents("https://geo.api.gouv.fr/communes/".$iris_json->properties->citycode."?fields=nom,codesPostaux,codeDepartement,departement,codeRegion,region,population&format=geojson&geometry=contour"));
+        $data['geo']['geo_json']['commune'] = $commune_json;
+
+        $departements = json_decode(file_get_contents("https://raw.githubusercontent.com/gregoiredavid/france-geojson/master/departements.geojson"));
+        foreach($departements->features as $f) {
+            if ($f->properties->code == $commune_json->properties->codeDepartement) {
+                $data['geo']['geo_json']['departement'] = $f;
+                break;
+            }
+        }
+        $regions = json_decode(file_get_contents("https://raw.githubusercontent.com/gregoiredavid/france-geojson/master/regions.geojson"));
+        foreach($regions->features as $f) {
+            if ($f->properties->code == $commune_json->properties->codeRegion) {
+                $data['geo']['geo_json']['region'] = $f;
+                break;
+            }
+        }
+
+        print_r(json_encode($data));
     }
 }
