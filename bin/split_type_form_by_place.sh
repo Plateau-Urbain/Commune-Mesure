a484bec7516b4bfc5a6695e803df8b6e28caab46
Honorine-MC <honorine.mariechristy@gmail.com>
2021-08-30 09:55:43 +0200

    verifie que le fichier existe bien avant de faire le split par lieu

--- bin/split_type_form_by_place.sh
+++ bin/split_type_form_by_place.sh
@@ -7,8 +7,13 @@ TAILLEJSON=$(($NOMBREDELIEU-1))
 
 mkdir -p "$CHEMIN"/jsonFromTypeForm
 
-for i in `seq 0  $TAILLEJSON`;
-do
-  FICHIER=$(jq .[$i] "$1" |jq  .answers[1].group.answers[0].short_text.value | sed "s/\"//g" |sed "s/ /_/g"| sed "s#/#_#g")
-  jq .[$i] "$1"  > "$CHEMIN"/jsonFromTypeForm/$FICHIER.json
-done
+if [ -e $1 ]
+then
+  for i in `seq 0  $TAILLEJSON`;
+  do
+    FICHIER=$(jq .[$i] "$1" |jq  .answers[1].group.answers[0].short_text.value | sed "s/\"//g" |sed "s/ /_/g"| sed "s#/#_#g")
+    jq .[$i] "$1"  > "$CHEMIN"/jsonFromTypeForm/$FICHIER.json
+  done
+else
+  echo "Le fichier json n'existe pas"
+fi
