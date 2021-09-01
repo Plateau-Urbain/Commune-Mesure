bfcccc54ca30f12713ec0c02efcf5df84426aaad
Honorine-MC <honorine.mariechristy@gmail.com>
2021-09-01 19:05:23 +0200

    verfification de l'existance du fichier csv pour avoir les données iris ...

--- bin/import.sh
+++ bin/import.sh
@@ -2,6 +2,7 @@
 
 #appel script puppeteer
 DOSSIER_PUPPETEER="../puppeteer_scripts/"
+CHEMIN_IRIS_CSV="storage/framework/cache/data/"
 node "$DOSSIER_PUPPETEER"typerform.js
 
 #appel script split
@@ -10,5 +11,16 @@ for file in "$DOSSIER_PUPPETEER"out/*.json; do   [[ $file -nt $FICHIER_TYPE_FORM
 echo $FICHIER_TYPE_FORM
 bash bin/split_type_form_by_place.sh "$FICHIER_TYPE_FORM"
 
-#task import
-#verifier si le fichier .csv pour le geojson existe sinon telecharger le zip pui dezipper puis for i in /sto/.../*.json
+#fichier pour iris ...
+if [ -e "$CHEMIN_IRIS_CSV"base-ic-evol-struct-pop-2016.csv ]
+then
+  do
+    echo "Le fichier base-ic-evol-struct-pop-2016.csv existe"
+  done
+else
+  wget https://www.insee.fr/fr/statistiques/fichier/4228434/base-ic-evol-struct-pop-2016.zip -O "$CHEMIN_IRIS_CSV"base-ic-evol-struct-pop-2016.zip
+  unzip "$CHEMIN_IRIS_CSV"base-ic-evol-struct-pop-2016.zip -d "$CHEMIN_IRIS_CSV"
+  rm base-ic-evol-struct-pop-2016.zip
+fi
+
+# tache d'import
