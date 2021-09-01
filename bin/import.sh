42e04996c6ee2ae42f6c033ba9cf75e3eb98bd49
Honorine-MC <honorine.mariechristy@gmail.com>
2021-09-01 19:31:41 +0200

    import global mais il faut gerer le yes à ecrire à chaque fois

--- bin/import.sh
+++ bin/import.sh
@@ -3,20 +3,19 @@
 #appel script puppeteer
 DOSSIER_PUPPETEER="../puppeteer_scripts/"
 CHEMIN_IRIS_CSV="storage/framework/cache/data/"
-node "$DOSSIER_PUPPETEER"typerform.js
+CHEMIN='storage/app/'
+
+node "$DOSSIER_PUPPETEER"typeform.js
 
 #appel script split
 unset -v FICHIER_TYPE_FORM
 for file in "$DOSSIER_PUPPETEER"out/*.json; do   [[ $file -nt $FICHIER_TYPE_FORM ]] && FICHIER_TYPE_FORM=$file; done
-echo $FICHIER_TYPE_FORM
 bash bin/split_type_form_by_place.sh "$FICHIER_TYPE_FORM"
 
 #fichier pour iris ...
-if [ -e "$CHEMIN_IRIS_CSV"base-ic-evol-struct-pop-2016.csv ]
+if [ -e "$CHEMIN_IRIS_CSV"base-ic-evol-struct-pop-2016.csv ];
 then
-  do
-    echo "Le fichier base-ic-evol-struct-pop-2016.csv existe"
-  done
+  echo "Le fichier base-ic-evol-struct-pop-2016.csv existe"
 else
   wget https://www.insee.fr/fr/statistiques/fichier/4228434/base-ic-evol-struct-pop-2016.zip -O "$CHEMIN_IRIS_CSV"base-ic-evol-struct-pop-2016.zip
   unzip "$CHEMIN_IRIS_CSV"base-ic-evol-struct-pop-2016.zip -d "$CHEMIN_IRIS_CSV"
@@ -24,3 +23,7 @@ else
 fi
 
 # tache d'import
+for i in "$CHEMIN"jsonFromTypeForm/*.json
+  do
+    php artisan import:typeform "$i"
+  done
