d5ed1007ff7052118724bafa25acaf5f15831b82
Gabriel Poma <gpoma@24eme.fr>
2021-09-02 19:18:28 +0200

    mini fix scripts import / split

--- bin/split_type_form_by_place.sh
+++ bin/split_type_form_by_place.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
-CHEMIN='storage/app'
+CHEMIN='storage/import'
+
+if [ -z "$1" ]
+then
+    echo "Need json file as parameter"
+    exit 1
+fi
 
 NOMBREDELIEU=$(jq length "$1")
 TAILLEJSON=$(($NOMBREDELIEU-1))
 
-mkdir -p "$CHEMIN"/jsonFromTypeForm
-
-if [ -e $1 ]
+if [ ! -d "$CHEMIN" ]
 then
-  for i in `seq 0  $TAILLEJSON`;
-  do
-    FICHIER=$(jq .[$i] "$1" |jq  .answers[1].group.answers[0].short_text.value | sed "s/\"//g" |sed "s/ /_/g"| sed "s#/#_#g" | sed "s#'#_#g")
-    jq .[$i] "$1"  > "$CHEMIN"/jsonFromTypeForm/$FICHIER.json
-  done
-else
-  echo "Le fichier json n'existe pas"
+    mkdir -p "$CHEMIN"
 fi
+
+for i in $(seq 0 $TAILLEJSON);
+do
+    FICHIER=$(jq .[$i] "$1" | jq .answers[1].group.answers[0].short_text.value | sed "s/\"//g" |sed "s/ /_/g"| sed "s#/#_#g" | sed "s#'#_#g")
+    jq .[$i] "$1" > "$CHEMIN"/"$FICHIER".json
+done
