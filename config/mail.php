65dedc07b0ffba4bb9e1340e7dd1c9f5582aa8ae
Gabriel Poma <gpoma@24eme.fr>
2021-09-03 19:45:57 +0200

    mailgun config

--- config/mail.php
+++ config/mail.php
@@ -51,6 +51,9 @@ return [
 
         'mailgun' => [
             'transport' => 'mailgun',
+            'domain' => env('MAILGUN_DOMAIN'),
+            'secret' => env('MAILGUN_SECRET'),
+            'endpoint' => env('MAILGUN_ENDPOINT', 'api.eu.mailgun.net'),
         ],
 
         'postmark' => [
