abc1a2cfecf8dbe4d3ba1a262bdf946fce3c1dc6
Gabriel Poma <gpoma@24eme.fr>
2021-09-07 17:26:25 +0200

    fix css banner edit

--- resources/views/place/show.blade.php
+++ resources/views/place/show.blade.php
@@ -27,38 +27,38 @@
 
 @section('content')
   @isset($edit)
-  <div style="position: fixed;width: 100%;top: 50px; z-index: 999;display:inline-flex" class="has-background-warning">
+  <div class="has-background-warning edit-banner">
     <div class="container">
-      <p class="has-text-centered has-text-weight-bold py-2" style="margin:auto;">
+      <p class="has-text-centered has-text-weight-bold py-2">
       Vous êtes en mode édition. Revenir à la <a href="{{ route('place.show', ['slug' => $slug]) }}">page consultation du lieu</a>.
       </p>
     </div>
     <button
       <?php if($place->isPublish()):?>
           class="button is-danger is-light" title="Dé-publier le lieu">
-          <a style='color: black'href="{{ route('place.publish', ['slug' => $slug, 'auth' => $auth]) }}">
+          <a class="has-text-black" href="{{ route('place.publish', ['slug' => $slug, 'auth' => $auth]) }}">
             <span class="icon">
-              <i class="fas fa-users-slash"></i>
+              <i class="fa fa-users-slash"></i>
             </span>
           </a>
       <?php else : ?>
         class="button" title="Publier le lieu">
-        <a style='color: black'href="{{ route('place.publish', ['slug' => $slug, 'auth' => $auth]) }}">
+        <a class="has-text-black" href="{{ route('place.publish', ['slug' => $slug, 'auth' => $auth]) }}">
           <span class="icon">
-            <i class="fas fa-globe"></i>
+            <i class="fa fa-globe"></i>
           </span>
         </a>
       <?php endif ?>
     </button>
-    <a style='color: white' href="{{ route('place.csv', ['slug' => $slug, 'auth' => $auth]) }}">
-      <button style='margin: 0px 20px 0px 20px;'class="button is-success" title="Télécharger le csv">
+    <a href="{{ route('place.csv', ['slug' => $slug, 'auth' => $auth]) }}">
+      <button class="button is-success ml-4" title="Télécharger le csv">
         <span class="icon">
-          <i class="fas fa-download"></i>
+          <i class="fa fa-download"></i>
         </span>
       </button>
     </a>
 
-    <span style="padding: 5px 20px 0px 20px;color:red;float:right" class=" has-text-right"><a href="{{ route('place.show', ['slug' => $slug]) }}"><i class='fas fa-times'></i></a></span>
+    <span><a class="has-text-primary mx-4" href="{{ route('place.show', ['slug' => $slug]) }}"><i class='fas fa-times'></i></a></span>
   </div>
 @endisset
 
