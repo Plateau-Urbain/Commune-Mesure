cdbdf920d367afa9872df5e3cf1f936868947aa3
Gabriel Poma <gpoma@24eme.fr>
2021-12-28 17:46:44 +0100

    lieu ouvert depuis / ouverture prévue

--- resources/views/partials/place/sections/bloc-milieu.blade.php
+++ resources/views/partials/place/sections/bloc-milieu.blade.php
@@ -9,8 +9,11 @@
       <div class="window very-small edit-milieu">{{ $place->get('blocs->presentation->donnees->nombre_occupants') }} structures occupantes @include('components.modals.modalEdition',['chemin'=>'blocs->presentation->donnees->nombre_occupants','id_section'=>'presentation','type' => 'number','titre'=>"Modifier le nombre de structures occupantes",'description' =>"Le nombre de structures exerçant leur activité ou  ayant leurs  locaux au sein du lieu"])</div>
       <div class="window very-small edit-milieu">La gouvernance partagée avec {{ $place->get('blocs->presentation->donnees->noms_occupants') }} @include('components.modals.modalEdition',['chemin'=>'blocs->presentation->donnees->noms_occupants','id_section'=>'presentation','type' => 'text','titre'=>"Modifier la gouvernance partagée","description" =>"Les différentes structures impliquées en cas de gouvernance partagée du lieu "])</div>
       <div class="window very-small edit-milieu">
-        Ouvert depuis le {{ \Carbon\Carbon::create($place->get('blocs->presentation->donnees->date_ouverture'))->locale(config('app.locale'))->isoFormat("LL") }}
-        @include('components.modals.modalEdition',['chemin'=>'blocs->presentation->donnees->date_ouverture','id_section'=>'presentation','type' => 'date','titre'=>"Modifier la date d'ouverture",'description'=>"La date d'ouverture du lieu"])
+        @php $date = \Carbon\Carbon::create($place->get('blocs->presentation->donnees->date_ouverture')) @endphp
+        @php $now = \Carbon\Carbon::now(); @endphp
+        @if ($date < $now) Ouvert depuis le @else Ouverture prévue le @endif
+        {{ \Carbon\Carbon::create($place->get('blocs->presentation->donnees->date_ouverture'))->locale(config('app.locale'))->isoFormat("LL") }}
+        @include('components.modals.modalEdition', ['chemin' => 'blocs->presentation->donnees->date_ouverture', 'id_section' => 'presentation', 'type' => 'date', 'titre' => "Modifier la date d'ouverture", 'description'=>"La date d'ouverture du lieu"])
       </div>
       <div class="window very-small edit-milieu">Surface de {{ $place->get('blocs->presentation->donnees->surface') }}m<sup>2</sup>  @include('components.modals.modalEdition',['chemin'=>'blocs->presentation->donnees->surface','id_section'=>'presentation','type' => 'decimal','titre'=>"Modifier la surface",'description'=>"La superficie du lieu en m2"])</div>
       <div class="window very-small edit-milieu">{{ $place->get('blocs->presentation->donnees->emplois directs') }}
