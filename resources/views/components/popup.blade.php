1889a19377888caf9fedc81ef2f6a701ad759152
Gabriel Poma <gpoma@24eme.fr>
2022-02-14 16:36:09 +0100

    popup: nom correct dans la popup

--- resources/views/components/popup.blade.php
+++ resources/views/components/popup.blade.php
@@ -11,7 +11,7 @@
 
     <div class='card-content'>
         <div class='content'>
-            <p class='title is-4'><a href='{{ route('place.show', ['slug' => $name]) }}'>{{ $name }}</a></p>
+            <p class='title is-4'><a href='{{ route('place.show', ['slug' => $name]) }}'>{{ $title }}</a></p>
             <p class='subtitle is-6'>{{ $city }} ({{ $departement }})</p>
             <p>{{ $description }}</p>
             <br/><br/>
