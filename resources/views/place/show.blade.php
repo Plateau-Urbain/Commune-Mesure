efa22d322e47c5c4e402403ff7a9a2ddcb5fe307
Gabriel Poma <gpoma@24eme.fr>
2022-02-25 19:54:31 +0100

    place: fix maison en double impression

--- resources/views/place/show.blade.php
+++ resources/views/place/show.blade.php
@@ -112,7 +112,7 @@
           </p>
         </div>
         <div class="section pt-5" style="padding-bottom:0;">
-          <div class="columns is-centered is-hidden-desktop is-hidden-mobile">
+          <div class="columns is-centered is-hidden-desktop is-hidden-mobile is-hidden-print">
             <div class="column is-half">
               @include('partials.place.sections.bloc-milieu')
             </div>
