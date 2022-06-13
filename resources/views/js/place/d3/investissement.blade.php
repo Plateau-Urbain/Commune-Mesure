d9f8ad34f18b702fe734bbb4b372bdb30b59203f
Gabriel Poma <gpoma@24eme.fr>
2022-06-13 17:43:54 +0200

    d3: graph investissement dans page lieu

--- resources/views/js/place/d3/investissement.blade.php
+++ resources/views/js/place/d3/investissement.blade.php
@@ -1,48 +1,35 @@
 <script type="text/javascript">
   const svg_investissement_id = 'svg#investissement-graph'
+  const svg = d3.select('svg')
 
-  let parea_fond_data = [
-    {{ $place->get('blocs->moyens->donnees->investissement->Fonds privés') ?: 0 }},
-    {{ $place->get('blocs->moyens->donnees->investissement->Fonds apportés') ?: 0 }},
-    {{ $place->get('blocs->moyens->donnees->investissement->Fonds publics') ?: 0 }}
+  const parea_data = [
+      {name: "Fonds privés", value: {{ $place->get('blocs->moyens->donnees->investissement->Fonds privés') ?: 0 }}},
+      {name: "Fonds propres", value: {{ $place->get('blocs->moyens->donnees->investissement->Fonds apportés') ?: 0 }}},
+      {name: "Fonds publics", value: {{ $place->get('blocs->moyens->donnees->investissement->Fonds publics') ?: 0 }}}
   ]
-  let parea_fond_name = [
-    'Fonds privés',
-    'Fonds propres',
-    'Fonds publics',
-  ]
-  let width = 500;
-  let height = 500;
 
-  let total = 0;
-  parea_data = [];
-  for(let k in parea_fond_name) {
-    total += parea_fond_data[k];
-    parea_data.push({'name': parea_fond_name[k], 'value': parea_fond_data[k] });
-  }
-  parea_fond_data.sort();
-  parea_fond_data.reverse();
-  total -= parea_fond_data[0];
+  const width = svg.node().getBoundingClientRect().width;
+  const height = svg.node().getBoundingClientRect().height;
+  const total = parea_data.reduce((accum, item) => accum + item.value, 0);
 
-  for(let i = 0 ; i < parea_data.length ; i ++) {
-    parea_data[i]['pc'] = parea_data[i]['value'] / total;
-    parea_data[i]['id'] = i;
-  }
+  const color = d3.scaleOrdinal()
+      .domain(parea_data.map(d => d.name))
+      .range(["#1b9e77","#d95f02","#7570b3","#e7298a","#66a61e","#e6ab02","#a6761d","#666666"])
+
+  parea_data.forEach(function (element, index) {
+      element['pc'] = element.value / total
+      element['id'] = index
+      element['width'] = height * element['pc'];
+      element['height'] = height * element['pc'];
+      element['fill'] = color(index)
+  })
+
   parea_data[0]['x'] =  10;
   parea_data[0]['y'] =  height - height * parea_data[0]['pc'];
-  parea_data[0]['width'] =  height * parea_data[0]['pc'];
-  parea_data[0]['height'] =  height * parea_data[0]['pc'];
-  parea_data[0]['fill'] =  '#ff5728';
   parea_data[1]['x'] = 20 + height * parea_data[0]['pc'];
   parea_data[1]['y'] = height - height * parea_data[1]['pc'];
-  parea_data[1]['width'] = height * parea_data[1]['pc'];
-  parea_data[1]['height'] = height * parea_data[1]['pc'];
-  parea_data[1]['fill'] = '#1abc9c';
   parea_data[2]['x'] = parea_data[1]['x'];
   parea_data[2]['y'] = - 10 + parea_data[1]['y'] - height * parea_data[2]['pc'] ;
-  parea_data[2]['width'] = height * parea_data[2]['pc'];
-  parea_data[2]['height'] = height * parea_data[2]['pc'];
-  parea_data[2]['fill'] = '#96043e';
 
   d3.select(svg_investissement_id)
     .selectAll('rect')
