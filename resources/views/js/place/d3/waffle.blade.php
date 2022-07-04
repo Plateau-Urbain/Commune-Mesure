2e7defbfd791d81148af89c028fa552f1dbc9167
Gabriel Poma <gpoma@24eme.fr>
2022-07-04 12:51:56 +0200

    wip waffle

--- resources/views/js/place/d3/waffle.blade.php
+++ resources/views/js/place/d3/waffle.blade.php
@@ -1,40 +1,56 @@
 <script type="text/javascript">
   const tooltip_id = 'tooltip-waffle';
   const svg_id = 'svg#waffle';
+  const svg_waffle = d3.select(svg_id)
 
-  let waffle_structure_data = {
-    'entreprise': {{ $place->get('blocs->composition->donnees->type->Entreprises') ?: 0 }},
-    'associations': {{ $place->get('blocs->composition->donnees->type->Associations') ?: 0 }},
-    'artistes': {{ $place->get('blocs->composition->donnees->type->Artistes') ?: 0 }},
-    'autres': {{ $place->get('blocs->composition->donnees->type->Autres structures') ?: 0 }}
-  }
-  let waffle_structure_text = {
-    'entreprise': ' entreprises',
-    'associations': ' associations',
-    'artistes': ' artistes',
-    'autres': ' autres'
-  }
-  let waffle_structure_color = {
-    'entreprise': d3.rgb('#deebee'),
-    'associations': d3.rgb('#ff5728'),
-    'artistes': d3.rgb('#1abc9c'),
-    'autres': d3.rgb('#96043e'),
-    'empty': d3.rgb('white')
+  const width_waffle = svg_waffle.node().getBoundingClientRect().width;
+
+  const waffle_structure = {
+    'entreprises': {
+      'data': {{ $place->get('blocs->composition->donnees->type->Entreprises') ?: 0 }},
+      'text': 'entreprises'
+    },
+    'associations': {
+      'data': {{ $place->get('blocs->composition->donnees->type->Associations') ?: 0 }},
+      'text': 'associations'
+    },
+    'artistes': {
+      'data': {{ $place->get('blocs->composition->donnees->type->Artistes') ?: 0 }},
+      'text': 'artistes'
+    },
+    'autres': {
+      'data': {{ $place->get('blocs->composition->donnees->type->Autres structures') ?: 0 }},
+      'text': 'autres'
+    },
+    'empty': {
+      'data': null,
+      'text': ''
+    }
   }
 
   let waffle_data = [];
-  for (let key of Object.keys(waffle_structure_data)) {
-    for(let i = 0 ; i < waffle_structure_data[key] ; i++) {
-      waffle_data.push(key);
-    }
-  }
-  width = 500;
-  carreau_num = Math.floor(Math.sqrt(waffle_data.length)) + 1;
-  carreau_size = Math.floor(width / carreau_num);
-  for(let i = waffle_data.length ; i < carreau_num * carreau_num ; i++) {
+  let total_structures = Object.values(waffle_structure).reduce((acc, val) => acc + val.data, 0)
+
+  color.domain(Object.keys(waffle_structure))
+
+  //for (let key of Object.keys(waffle_structure_data)) {
+  //  for(let i = 0 ; i < waffle_structure_data[key] ; i++) {
+  //    waffle_data.push(key);
+  //  }
+  //}
+  carreau_num = Math.floor(Math.sqrt(total_structures));
+  carreau_size = Math.floor(width_waffle / carreau_num);
+
+  Object.values(waffle_structure).forEach(function (el) {
+    const a = Array(el.data).fill(el.text, 0)
+    waffle_data.push(...a)
+  })
+
+  for(let i = total_structures ; i < carreau_num * carreau_num ; i++) {
     waffle_data.push('empty');
   }
 
+  //console.log(waffle_data)
   waffle_data.reverse();
 
   d3.select('body')
@@ -42,8 +58,7 @@
     .attr('id', tooltip_id)
     .attr('style', 'position: absolute; opacity: 0;');
 
-  d3.select(svg_id)
-    .selectAll('rect')
+  svg_waffle.selectAll('rect')
     .data(waffle_data)
     .enter()
     .append('rect')
@@ -56,18 +71,17 @@
       return (i % carreau_num) * carreau_size ;
     })
     .attr('y', function(d, i) {return Math.floor(i / carreau_num) * carreau_size })
-    .attr('fill', function(d) {if (waffle_structure_color[d]) {return waffle_structure_color[d]} return 'white';} )
+    .attr('fill', function(d) { return color(d) })
     .attr('class', function(d) {return 'waffle_'+d; })
     .on("mouseover", function(d) {
       d3.selectAll('.waffle_'+d)
-        .attr('fill', waffle_structure_color[d].brighter())
+        .attr('fill', (d) => color(d)+'55')
       ;
-      console.log(!isNaN(waffle_structure_data[d]) * 1);
       d3.select('#'+tooltip_id)
-        .style('opacity', ! isNaN(waffle_structure_data[d]) * 1 )
+        .style('opacity', ! isNaN(waffle_structure[d]) * 1 )
         .text( function(a) {
-          if (waffle_structure_data[d])
-            return waffle_structure_data[d]+waffle_structure_text[d];
+          if (waffle_structure[d])
+            return waffle_structure[d].data+waffle_structure[d].text;
         })
     } )
     .on('mousemove', function(d) {
@@ -77,7 +91,7 @@
     })
     .on("mouseout", function(d) {
       d3.selectAll('.waffle_'+d)
-        .attr('fill', waffle_structure_color[d])
+        .attr('fill', (d) => color(d))
       d3.select('#'+tooltip_id)
         .style('opacity', 0);
     } )
