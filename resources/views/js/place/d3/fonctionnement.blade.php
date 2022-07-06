2359e34dd1c75effb6da876b24bb400ed4bce706
Tangui Morlier <tangui@tangui.eu.org>
2022-07-06 19:30:01 +0200

    avec legende et mouseover

--- resources/views/js/place/d3/fonctionnement.blade.php
+++ resources/views/js/place/d3/fonctionnement.blade.php
@@ -1,10 +1,18 @@
 <script type="text/javascript">
   const svg_fonctionnement  = d3.select('svg#financement-budget-doughnut')
-  const width               = svg_fonctionnement.node().getBoundingClientRect().width
-  const height              = svg_fonctionnement.node().getBoundingClientRect().height
-  const margin              = 40
+  const fonctionnement_width               = svg_fonctionnement.node().getBoundingClientRect().width
+  const fonctionnement_height              = svg_fonctionnement.node().getBoundingClientRect().height - 100
+  const fonctionnement_margin              = 40
 
-  const radius = Math.min(width, height) / 2 - margin
+  const fonctionnement_radius = Math.min(fonctionnement_width, fonctionnement_height) / 2 - fonctionnement_margin
+
+  const tooltip_fonctionnement_id = 'tooltip-fonctionnement';
+
+  d3.select('body')
+    .append('div')
+    .attr('id', tooltip_fonctionnement_id)
+    .attr('class', 'd3_tooltip')
+    .attr('style', 'position: absolute; opacity: 0;');
 
   const donut_data = [
     {name: "Recettes", count: {{ $place->get('blocs->moyens->donnees->fonctionnement->Recettes') }} },
@@ -17,33 +25,58 @@
   const pieArcData = d3.pie()
     .value(d => d.count)(donut_data)
   const arc = d3.arc()
-    .innerRadius(radius * 0.5)
-    .outerRadius(radius * 0.8)
+    .innerRadius(fonctionnement_radius * 0.5)
+    .outerRadius(fonctionnement_radius * 0.8)
     .padRadius(200)
     .padAngle(5/300)
 
-  svg_fonctionnement.attr('viewBox', [-width / 2, -height / 2, width, height])
+  svg_fonctionnement
+    .attr('width', fonctionnement_width)
+    .attr('height', fonctionnement_height + 100)
     .append('g')
+    .attr("transform", "translate(" + fonctionnement_width / 2 + "," + fonctionnement_height / 2 + ")")
     .selectAll('path')
     .data(pieArcData)
     .enter()
     .append('path')
     .attr('d', (d) => arc(d))
     .attr('fill', (d) => color(d.data.name))
+    .on("mouseover", function(d) {
+      d3.select('#'+tooltip_fonctionnement_id)
+        .style('opacity', ! isNaN(d.data.count) * 1 )
+        .text( function(a) {
+          if (d.data.count)
+            return d.data.name+' : '+d.data.count+' €';
+        })
+    } )
+    .on('mousemove', function(d) {
+      d3.select('#'+tooltip_fonctionnement_id)
+        .style('left', (d3.event.pageX + 25) + 'px')
+        .style('top', (d3.event.pageY + 25) + 'px')
+    })
+    .on("mouseout", function(d) {
+      d3.select('#'+tooltip_fonctionnement_id)
+        .style('opacity', 0);
+    } )
 
-  svg_fonctionnement.append('g')
-    .attr("font-family", "sans-serif")
-    .attr("font-size", 26)
-    .attr("text-anchor", "middle")
-    .selectAll('text')
-    .data(pieArcData)
-    .enter()
-    .append('text')
-    .attr('transform', (d) => `translate(${arc.centroid(d).join(',')})`)
-    .attr('fill', "white")
-    .attr('stroke', 'black')
-    .attr('stroke-width', '1')
-    .text((d) => (d.data.count) ? d.data.name+' : \n'+d.data.count : '')
+    svg_fonctionnement
+      .selectAll('legend')
+      .data(pieArcData)
+      .enter()
+      .append('circle')
+      .attr('cx', function(d, i) { return 80 + (i % 2)* fonctionnement_width / 2.5 + 10})
+      .attr('cy', function(d, i) { return fonctionnement_height + 30 * (1 + Math.floor( i / 2 )) })
+      .attr('r', function(d) { return 10})
+      .attr('fill', function(d) { return color(d.data.name)})
+
+    svg_fonctionnement
+      .selectAll('legend-text')
+      .data(pieArcData)
+      .enter()
+      .append('text')
+      .attr('x', function(d, i) { return 100 + (i % 2)* fonctionnement_width / 2.5 + 10})
+      .attr('y', function(d, i) { return fonctionnement_height + 33 * (1 + Math.floor( i / 2 )) })
+      .attr('fill', 'black')
+      .text(function(d) { return d.data.name })
 
 </script>
-
