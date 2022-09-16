b52e6ffcf9ba73d3201003280c8362b221986486
Gabriel Poma <gpoma@24eme.fr>
2022-09-16 15:58:19 +0200

    html: on déplace sticky dans un partial a part

--- resources/views/place/show.blade.php
+++ resources/views/place/show.blade.php
@@ -62,43 +62,8 @@
 
 @section('content')
   @isset($edit)
-  <div class="has-background-warning edit-banner">
-    <div class="container">
-      <p class="has-text-centered has-text-weight-bold py-2">
-      Vous êtes en mode édition. Revenir à la <a href="{{ route('place.show', ['slug' => $slug]) }}">page consultation du lieu</a>.
-      </p>
-    </div>
-    <button class="button is-info mx-4" id="modal-help-btn" title="Aide" data-modal="modal-help">
-      <i class="fa fa-question"></i>
-    </button>
-    <button
-      <?php if($place->isPublish()):?>
-          class="button is-danger is-light" title="Dé-publier le lieu">
-          <a class="has-text-black" href="{{ route('place.publish', ['slug' => $slug, 'auth' => $auth]) }}">
-            <span class="icon">
-              <i class="fa fa-users-slash"></i>
-            </span>
-          </a>
-      <?php else : ?>
-        class="button" title="Publier le lieu">
-        <a class="has-text-black" href="{{ route('place.publish', ['slug' => $slug, 'auth' => $auth]) }}">
-          <span class="icon">
-            <i class="fa fa-globe"></i>
-          </span>
-        </a>
-      <?php endif ?>
-    </button>
-    <a href="{{ route('place.csv', ['slug' => $slug, 'auth' => $auth]) }}">
-      <button class="button is-success ml-4" title="Télécharger le csv">
-        <span class="icon">
-          <i class="fa fa-download"></i>
-        </span>
-      </button>
-    </a>
-
-    <span><a class="has-text-primary mx-4" href="{{ route('place.show', ['slug' => $slug]) }}"><i class='fas fa-times'></i></a></span>
-  </div>
-@endisset
+    @include('partials.place.sticky-edit')
+  @endisset
 
 <div class="columns is-gapless is-mobile" id="container">
 
