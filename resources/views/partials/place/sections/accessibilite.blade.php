f8a526338b008f38ea23ea2f0fc2b614be13ada0
Gabriel Poma <gpoma@24eme.fr>
2022-10-06 18:56:00 +0200

    icones accessibilité + public rework

--- resources/views/partials/place/sections/accessibilite.blade.php
+++ resources/views/partials/place/sections/accessibilite.blade.php
@@ -1,3 +1,10 @@
+@php
+  $accessibilite_icons = [
+    'Handicapés' => 'fauteuil-roulant',
+    'Mal voyants' => 'mal-voyant'
+  ];
+@endphp
+
 <h4 class="is-size-4 has-text-primary no-border is-uppercase">
   Accessibilité
   @include('components.modals.modalEdition',['chemin'=>'blocs->accessibilite->donnees->accessibilite','id_section'=>'accessibilite','type' => 'checkbox','titre' => "Modifier l'accessibilité",'description'=>"L'accessibilité du lieu aux personnes à mobilité réduite ou en situation de handicap"])
@@ -5,11 +12,11 @@
 
 <div class="columns is-multiline">
     @foreach($place->getAccessibilite() as $accessibilite => $check)
-      @if($accessibilite == 'Handicapés' && $check)
-        <span class="ml-3 public-icons has-tooltip-bottom" data-tooltip="Fauteuils roulants"><i class="cm-icons fauteuil-roulant font-color-theme mr-1"></i></span>
-      @endif
-      @if ($accessibilite == 'Mal voyants' && $check)
-        <span class="ml-3 public-icons has-tooltip-bottom" data-tooltip="Mal-voyants"><i class="cm-icons mal-voyant font-color-theme mr-1"></i></span>
+      @if($check )
+        <span class="ml-3 cm-icons-container column is-2">
+          <i class="cm-icons {{ $accessibilite_icons[$accessibilite] }} mr-1"></i>
+          <br/>{{ $accessibilite }}
+        </span>
       @endif
     @endforeach
 </div>
--- resources/views/partials/place/sections/public.blade.php
+++ resources/views/partials/place/sections/public.blade.php
@@ -1,3 +1,17 @@
+@php
+  $public_icons = [
+    'Enfants' => 'enfants',
+    'Étudiants' => 'student',
+    'Famille' => 'familles',
+    'Handicapés' => 'fauteuil-roulant',
+    'Chercheurs d\'emplois' => 'chercheur-emploi',
+    'Personnes âgées' => 'personnes-agees',
+    'Personnes habitant sur le site' => 'habitant',
+    'Personnes visitant le site' => 'visiteur',
+    'Personnes travaillant sur le site' => 'travailleur'
+  ];
+@endphp
+
 <h4 class="is-size-4 has-text-primary no-border is-uppercase">
 Publics
 @include('components.modals.modalEdition', ['chemin' => 'blocs->accessibilite->donnees->publics', 'id_section' => 'accessibilite','type' => 'checkbox','titre' => 'Modifier les différents publics','description'=>"Les différentes catégories de public accueillies au sein du lieu"])
@@ -5,32 +19,11 @@ Publics
 
 <div class="columns is-multiline">
     @foreach($place->getPublics() as $public => $check)
-      @if($public == 'Enfants' && $check )
-        <span class="is-block ml-3 public-icons has-tooltip-bottom" data-tooltip="Enfants"><i class="cm-icons enfants font-color-theme mr-1"></i></span>
-      @endif
-      @if($public == 'Étudiants' && $check)
-        <span class="is-inline-block ml-3 public-icons has-tooltip-bottom" data-tooltip="Étudiants"><i class="cm-icons student font-color-theme mr-1"></i></span>
-      @endif
-      @if($public == 'Famille' && $check)
-        <span class="is-block ml-3 public-icons has-tooltip-bottom" data-tooltip="Famille"><i class="cm-icons familles font-color-theme mr-1"></i></span>
-      @endif
-      @if($public == 'Handicapés' && $check)
-        <span class="ml-3 public-icons has-tooltip-bottom" data-tooltip="Handicapés"><i class="cm-icons fauteuil-roulant font-color-theme mr-1"></i></span>
-      @endif
-      @if($public == "Chercheurs d'emplois" && $check)
-        <span class="ml-3 public-icons has-tooltip-bottom" data-tooltip="Chercheurs d'emplois"><i class="cm-icons chercheur-emploi font-color-theme mr-1"></i></span>
-      @endif
-      @if($public == 'Personnes âgées' && $check)
-        <span class="ml-3 public-icons has-tooltip-bottom" data-tooltip="Personnes âgées"><i class="cm-icons personnes-agees font-color-theme mr-1"></i></span>
-      @endif
-      @if($public == 'Personnes habitant sur le site' && $check)
-        <span class="ml-3 public-icons has-tooltip-bottom" data-tooltip="Personnes habitant sur le site"><i class="cm-icons habitant font-color-theme mr-1"></i></span>
-      @endif
-      @if($public == 'Personnes visitant le site' && $check)
-        <span class="ml-3 public-icons has-tooltip-bottom" data-tooltip="Personnes visitant le site"><i class="cm-icons visiteur font-color-theme mr-1"></i></span>
-      @endif
-      @if($public == 'Personnes travaillant sur le site' && $check)
-        <span class="ml-3 public-icons has-tooltip-bottom" data-tooltip="Personnes travaillant sur le site"><i class="cm-icons travailleur font-color-theme mr-1"></i></span>
+      @if($check )
+        <span class="ml-3 cm-icons-container column is-2">
+          <i class="cm-icons {{ $public_icons[$public] }} mr-1"></i>
+          <br/>{{ $public }}
+        </span>
       @endif
     @endforeach
 </div>
