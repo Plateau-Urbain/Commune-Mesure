c2790bc3ec08c414c6817d36031c1ad39d0602aa
Gabriel Poma <gpoma@24eme.fr>
2022-10-17 18:53:18 +0200

    suppr directive blade inutile

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -12,24 +12,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Carbon::setLocale(config('app.locale'));
-
-        app('view');
-        $blade = app('blade.compiler');
-
-        $blade->directive('svg', function($arguments) {
-            // Funky madness to accept multiple arguments into the directive
-            list($path, $class) = array_pad(explode(',', trim($arguments, "() ")), 2, '');
-            $path = trim($path, "' ");
-            $class = trim($class, "' ");
-
-            // Create the dom document as per the other answers
-            $svg = new \DOMDocument();
-            $svg->load(resource_path($path));
-            $svg->documentElement->setAttribute("class", $class);
-            $output = $svg->saveXML($svg->documentElement);
-
-            return $output;
-        });
     }
 
     /**
