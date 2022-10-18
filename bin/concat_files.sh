f0e513135d50f49cf0a1e36fd9abb70e23b0c586
Gabriel Poma <gpoma@24eme.fr>
2022-10-18 18:19:20 +0200

    fix headers export global

--- bin/concat_files.sh
+++ bin/concat_files.sh
@@ -4,7 +4,7 @@
 EXPORT_DIR='storage/exports'
 
 # On concatene les exports des fichiers originaux (typeform)
-cat "Lieu;CategorieId;Categorie;QuestionId;Question;Type;Reponse" > "$EXPORT_DIR"/export_global_typeform.csv.tmp
+echo "Lieu;CategorieId;Categorie;QuestionId;Question;Type;Reponse" > "$EXPORT_DIR"/export_global_typeform.csv.tmp
 
 for file in "$EXPORT_DIR"/*.typeform.csv
 do
@@ -14,7 +14,7 @@ done
 mv "$EXPORT_DIR"/export_global_typeform.csv.tmp "$EXPORT_DIR"/00_export_global_typeform.csv
 
 # On concatene les exports des fichiers enregistrés en base
-cat "Lieu;CategorieId;QuestionId;Type;Reponse" > "$EXPORT_DIR"/export_global_genere.csv.tmp
+echo "Lieu;CategorieId;QuestionId;Type;Reponse" > "$EXPORT_DIR"/export_global_genere.csv.tmp
 
 for file in "$EXPORT_DIR"/*.genere.csv
 do
