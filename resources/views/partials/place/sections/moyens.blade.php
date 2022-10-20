2d0ce811fb2a608f5fc2a7de452811d2546d6f2a
Gabriel Poma <gpoma@24eme.fr>
2022-10-20 19:16:19 +0200

    moyen "responsive"

--- resources/views/partials/place/sections/moyens.blade.php
+++ resources/views/partials/place/sections/moyens.blade.php
@@ -6,29 +6,29 @@
 
       <div class="section"></div>
 
-      <div class="columns is-centered">
+      <div class="columns is-centered is-desktop has-text-centered mx-auto">
         @if(! $place->isEmptyFonctionnement() || isset($edit))
-          <div class="column is-half">
+          <div class="column is-half-desktop">
             <div class="is-flex is-flex-direction-column is-align-items-center">
               <h6 class="subtitle is-6">
                 Fonctionnement
                 @include('components.modals.modalEdition',['chemin'=>'blocs->moyens->donnees->fonctionnement','id_section'=>'moyens','type'=>'number','titre'=>"Modifier le fonctionnement","description"=>"Le budget annuel de fonctionnement du projet"])
               </h6>
 
-              <svg id="financement-budget-doughnut" width="470" height="470" aria-label="Répartition budget fonctionnement / investissement" role="img">
+              <svg id="financement-budget-doughnut" width="100%" height="470" aria-label="Répartition budget fonctionnement / investissement" role="img">
             </div>
           </div>
         @endif
 
         @if(! $place->isEmptyInvestissement() || isset($edit))
-          <div class="column is-half">
+          <div class="column is-half-desktop">
             <div class="is-flex is-flex-direction-column is-align-items-center">
               <h6 class="subtitle is-6">
                 Investissement
                 @include('components.modals.modalEdition',['chemin'=>'blocs->moyens->donnees->investissement','id_section'=>'moyens','type'=>'number','titre'=>"Modifier l'investissement",'description'=>"Le budget initial nécessaire au financement du projet et à l'ouverture du lieu (en %)"])
               </h6>
 
-              <svg id="investissement-graph" width="470" height="470" aria-label="Répartition investissement" role="img"></svg>
+              <svg id="investissement-graph" width="100%" height="470" aria-label="Répartition investissement" role="img"></svg>
             </div>
           </div>
         @endif
