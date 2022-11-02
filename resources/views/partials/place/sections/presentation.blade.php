955b8de3671641b5fd8c5c1664a1ed6c6cf8a244
Gabriel Poma <gpoma@24eme.fr>
2022-11-02 11:35:36 +0100

    batiment en entier mobile

--- resources/views/partials/place/sections/presentation.blade.php
+++ resources/views/partials/place/sections/presentation.blade.php
@@ -19,41 +19,9 @@
       <p class="mt-6">
         @include('partials.place.reseaux-sociaux')
       </p>
-
-      <div class="has-text-centered mt-4 is-relative">
-
-        <x-svg :path="'assets/images/batiment/'.$batiment->getToit('gauche').'.svg'" class="is-block" transform="" width="100%" height="240" />
-
-        @for ($i = 0; $i < 3; $i++)
-          <svg height="240" class="is-block mx-auto" style="z-index: 100; position: relative;">
-            @php $t = $batiment->getThematique($i); @endphp
-            @php $part = (strpos($t, 'THEME_') === false) ? 'THEME_VIERGE' : $t; @endphp
-            <x-svg :path="'assets/images/batiment/themes/'.$part.'.svg'" class="" transform="" width="100%" height="100%">
-
-              @if (strpos($t, 'THEME_') === false)
-                @if (mb_strlen($t) > 30)
-                  <text x="50%" dominant-baseline="middle" text-anchor="middle" style="font-size:3rem;font-weight:bold;fill:#c9514a;">
-                  <tspan x="50%" dy="-1.2em">{{ strtok(mb_strtoupper($t), ' ') }} {{ strtok(' ') }} {{ strtok(' ') }}</tspan>
-                  @while($text = strtok(' '))
-                    <tspan x="50%" dy="1.2em">{{ $text }} {{ strtok(' ') }} {{ strtok(' ') }}</tspan>
-                  @endwhile
-                  </text>
-                @else
-                  <text x="50%" dominant-baseline="middle" text-anchor="middle" style="font-size:3rem;font-weight:bold;fill:#c9514a;" >
-                    {{ mb_strtoupper($t) }}
-                  </text>
-                @endif
-              @endif
-
-            </x-svg>
-          </svg>
-        @endfor
-        <div style="position: absolute; bottom: 16px; height: 3px; background: #262631; width: 90%; left: 5%;"></div>
-        <div style="position: absolute; bottom: 2px; height: 1px; background: #262631; width: 90%; left: 5%;"></div>
-      </div>
-
     </div>
-    <div class="is-hidden-mobile column is-6 is-offset-1">
+
+    <div class="column is-6 is-offset-1">
       @include('partials.place.sections.batiment')
     </div>
 
