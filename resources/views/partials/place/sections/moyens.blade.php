9610358b10384693e303c02a9d1a1ad41816ecc1
Gabriel Poma <gpoma@24eme.fr>
2022-11-22 13:20:11 +0100

    alignement titres moyens

--- resources/views/partials/place/sections/moyens.blade.php
+++ resources/views/partials/place/sections/moyens.blade.php
@@ -4,34 +4,27 @@
       <h4 class="title has-text-primary no-border has-text-weight-normal is-uppercase">Les moyens</h4>
       <p class="mb-6">Les moyens financiers mis en oeuvre pour assurer le fonctionnement du lieu.</p>
 
-      <div class="columns is-centered is-desktop mx-auto">
         @if(! $place->isEmptyFonctionnement() || isset($edit))
-          <div class="column">
-            <div class="is-flex is-flex-direction-column">
-              <h6 class="subtitle is-5 is-uppercase has-text-weight-bold mb-0">
-                Fonctionnement
-                @include('components.modals.modalEdition',['chemin'=>'blocs->moyens->donnees->fonctionnement','id_section'=>'moyens','type'=>'number','titre'=>"Modifier le fonctionnement","description"=>"Le budget annuel de fonctionnement du projet"])
-              </h6>
+          <div class="is-flex is-flex-direction-column">
+            <h6 class="subtitle is-5 is-uppercase has-text-weight-bold mb-0">
+              Fonctionnement
+              @include('components.modals.modalEdition',['chemin'=>'blocs->moyens->donnees->fonctionnement','id_section'=>'moyens','type'=>'number','titre'=>"Modifier le fonctionnement","description"=>"Le budget annuel de fonctionnement du projet"])
+            </h6>
 
-              <svg id="financement-budget-doughnut" width="100%" height="300" aria-label="Répartition budget fonctionnement / investissement" role="img">
-            </div>
+            <svg id="financement-budget-doughnut" width="100%" height="300" aria-label="Répartition budget fonctionnement / investissement" role="img">
           </div>
         @endif
-        </div>
-  <div class="columns is-centered is-desktop mx-auto">
+
         @if(! $place->isEmptyInvestissement() || isset($edit))
-          <div class="column is-half-mobile">
-            <div class="is-flex is-flex-direction-column">
-              <h6 class="subtitle is-5 is-uppercase has-text-weight-bold mb-0">
-                Investissement
-                @include('components.modals.modalEdition',['chemin'=>'blocs->moyens->donnees->investissement','id_section'=>'moyens','type'=>'number','titre'=>"Modifier l'investissement",'description'=>"Le budget initial nécessaire au financement du projet et à l'ouverture du lieu (en %)"])
-              </h6>
+          <div class="is-flex is-flex-direction-column">
+            <h6 class="subtitle is-5 is-uppercase has-text-weight-bold mb-0">
+              Investissement
+              @include('components.modals.modalEdition',['chemin'=>'blocs->moyens->donnees->investissement','id_section'=>'moyens','type'=>'number','titre'=>"Modifier l'investissement",'description'=>"Le budget initial nécessaire au financement du projet et à l'ouverture du lieu (en %)"])
+            </h6>
 
-              <svg id="investissement-graph" width="100%" height="470" aria-label="Répartition investissement" role="img"></svg>
-            </div>
+            <svg id="investissement-graph" width="100%" height="470" aria-label="Répartition investissement" role="img"></svg>
           </div>
         @endif
-      </div>
     </div>
   </div>
 </section>
