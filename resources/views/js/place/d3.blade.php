d7f71fe0901c50703a60bf3f9f6265ee99102bda
eoliamalherbe <118894187+eoliamalherbe@users.noreply.github.com>
2022-11-23 11:58:55 +0100

    Update d3.blade.php

--- resources/views/js/place/d3.blade.php
+++ resources/views/js/place/d3.blade.php
@@ -1,7 +1,7 @@
 <script type="text/javascript" src="https://d3js.org/d3.v4.min.js"></script>
 <script>
   const color = d3.scaleOrdinal()
-                  .range(['#C7514D', '#EA7B6C', '#E9C3B7', '#DDEBEF', '#7E97A7', '#B5CAB5', '#6C9871', '#262531']);
+                  .range(['#C7514D', '#EA7B6C', '#E9C3B7', '#DDEBEF', '#7E97A7', '#B5CAB5', '#6C9871', '#8F8E92']);
 </script>
 
 @include('js.place.d3.waffle')
