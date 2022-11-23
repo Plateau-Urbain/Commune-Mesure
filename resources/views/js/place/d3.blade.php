81f2f20751b430811b23a27bff9bffda442c1d02
Vincent LAURENT <vince.laurent@gmail.com>
2022-11-23 11:51:02 +0100

    Cette variable ne sert à rien

--- resources/views/js/place/d3.blade.php
+++ resources/views/js/place/d3.blade.php
@@ -2,7 +2,6 @@
 <script>
   const color = d3.scaleOrdinal()
                   .range(['#C7514D', '#EA7B6C', '#E9C3B7', '#DDEBEF', '#7E97A7', '#B5CAB5', '#6C9871', '#262531']);
-  const color2 = d3.scaleOrdinal().range(['#C7514D', '#EA7B6C', '#DDEBEF']);
 </script>
 
 @include('js.place.d3.waffle')
