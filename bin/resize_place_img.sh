7e3ab9cf3d6298b0745fc38b3d570c3a7edff7fd
Vincent LAURENT <vince.laurent@gmail.com>
2022-11-24 02:23:20 +0100

    Boulette

--- bin/resize_place_img.sh
+++ bin/resize_place_img.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ls public/images/lieux/ | grep "\..+" |  while read file; do convert "public/images/lieux/$file" -resize 400x "public/images/lieux/thumbnail/$file"; done
+ls public/images/lieux/ | grep -E "\..+" |  while read file; do convert "public/images/lieux/$file" -resize 400x "public/images/lieux/thumbnail/$file"; done
