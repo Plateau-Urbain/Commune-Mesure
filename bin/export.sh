ceceb4becdceaa0b42066888c637cd7a45ac9d84
Honorine-MC <honorine.mariechristy@gmail.com>
2022-11-29 16:21:20 +0100

    concatene toutes les images en une image

--- bin/export.sh
+++ bin/export.sh
@@ -12,13 +12,16 @@ PLACE=$1
 TOP=$2
 
 cd "$PUPPETEER_DIR" || exit
+
 node screenshot.js "$PLACE" "$TOP"
 _NODE_SUCCESS=$?
-# cd -
 
 if [ $_NODE_SUCCESS -eq 1 ] # Si 404 / 500
     then echo "Place $PLACE not found" && exit 1
 fi
 
+convert $(find "$SCREENSHOT_DIR" -name "$PLACE*.jpg" | sort) -append "$SCREENSHOT_DIR/$PLACE.jpg"
+
 echo "$SCREENSHOT_DIR/$PLACE.jpg"
+
 exit 0
