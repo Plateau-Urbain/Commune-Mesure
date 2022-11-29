dfcc1d2cc98476ef682d4d3089fc019bcb6aa2cf
Honorine-MC <honorine.mariechristy@gmail.com>
2022-11-29 19:02:40 +0100

    blocs acteurices et valeurs sont distinct

--- app/Models/Place.php
+++ app/Models/Place.php
@@ -313,7 +313,7 @@ class Place extends Model
   }
 
   public function getVisibility(){
-    $tabSections = ["presentation","accessibilite","valeurs","moyens","composition","impact_social","data_territoire","galerie"];
+    $tabSections = ["presentation","accessibilite","valeurs","moyens","composition","impact_social","data_territoire","galerie","acteurices"];
     $tabVisibility=array();
     foreach($tabSections as $s){
       $tabVisibility[$s]= $this->get('blocs->'.$s.'->visible');
@@ -332,7 +332,7 @@ class Place extends Model
   }
 
   public function getIsEmpty(){
-    $tabSections = ["accessibilite","valeurs","moyens","composition","impact_social","galerie"];
+    $tabSections = ["accessibilite","valeurs","moyens","composition","impact_social","galerie","acteurices"];
     $tabIsEmpty=array();
     foreach($tabSections as $s){
       $tabIsEmpty[$s]= $this->isEmpty($s);
@@ -380,6 +380,10 @@ class Place extends Model
     return true;
   }
 
+  public function isEmptyActeurices(){
+      return false;
+  }
+
   public function isEmpty($section){
     if($section == "moyens"){
       if($this->isEmptyFonctionnement() && $this->isEmptyInvestissement()){
@@ -390,6 +394,10 @@ class Place extends Model
 
     if($section == "accessibilite"){  //cas particulier 0/1
       return $this->isEmptyAccessibility();
+    }
+
+    if($section == "acteurices"){
+      return $this->isEmptyActeurices();
     }
 
     $tab = $this->get('blocs->'.$section.'->donnees');
