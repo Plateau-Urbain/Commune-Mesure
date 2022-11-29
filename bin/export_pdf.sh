2d4c47385ba99a2c09631f23f99fa07a9dfa4b78
Honorine-MC <honorine.mariechristy@gmail.com>
2022-11-29 16:21:57 +0100

    export pdf qui prend les images par sections

--- bin/export_pdf.sh
+++ bin/export_pdf.sh
@@ -1,23 +1,32 @@
 #!/bin/bash
+PUPPETEER_DIR="../puppeteer_scripts"
+SCREENSHOT_DIR="$PUPPETEER_DIR/out/screenshot"
 
 if [ $# -eq 0 ]
-    then echo "Missing file" && exit 255
-fi
-
-FILE=$1
-
-if [ ! -f "$FILE" ]
-    then echo "Screenshot file $FILE does not exists" && exit 128
+    then echo "Missing place name" && exit 255
 fi
 
 if ! command -v convert &> /dev/null
     then echo "Missing convert utils" && exit 64
 fi
 
-PDF_FILE="$(dirname "${FILE}")/$(basename "${FILE}" .jpg).pdf"
+PLACE=$1
 
-if ! convert "$FILE" -quality 95 "$PDF_FILE"
-    then echo "Conversion to pdf failed" && exit 32
-fi
+find "$SCREENSHOT_DIR/" -type f -iname "$PLACE*.pdf" -delete #supprime tous les pdfs du dossier
 
-echo "$PDF_FILE"
+find "$SCREENSHOT_DIR" -name "$PLACE*" | while read FILE;
+do
+    PDF_FILE="$(dirname "${FILE}")/$(basename "${FILE}" .jpg).pdf"
+    if ! convert "$FILE" -quality 95 "$PDF_FILE"
+        then echo "Conversion to pdf failed" && exit 32
+    fi
+done
+
+rm "$SCREENSHOT_DIR/$PLACE.pdf"
+
+pdftk $(find "$SCREENSHOT_DIR" -name "$PLACE*.pdf" | sort) output "$SCREENSHOT_DIR/$PLACE.pdf"
+
+
+echo "$SCREENSHOT_DIR/$PLACE.pdf"
+
+exit 0
